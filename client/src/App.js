41f084597061bad39afbf6e062645ba1346e6509
Jason Lam <nyjasonlam@gmail.com>
2021-06-23 11:45:30 -0400

    added home page and react routes to home and signup pages

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import Signup from './pages/Signup';
+import Home from './pages/Home'
 
 function App() {
   return (
     <>
-      <Signup />
+      <Router>
+        <Switch>
+          <Route exact path='/' component={Home} />
+          <Route exact path="/signup" component={Signup} />
+        </Switch>
+      </Router>
     </>
   );
 }
---
+++ client/src/pages/Home.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+const Home = () => {
+  return (
+    <>
+      <Link to='/signup'>Sign Up</Link>
+    </>
+  )
+}
+
+export default Home
