f8e2f24be7cf4a1e5efcbed83f554c01034d1a46
Jason Lam <nyjasonlam@gmail.com>
2021-06-23 15:24:14 -0400

    updated user signup mutation

--- server/server.js
+++ server/server.js
@@ -14,18 +14,20 @@ const server = new ApolloServer({
   context: authMiddleware,
 });
 
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../client/build')));
+}
+
 server.applyMiddleware({ app });
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/build')));
-}
 
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../client/build/index.html'));
-});
+
+// app.get('*', (req, res) => {
+//   res.sendFile(path.join(__dirname, '../client/build/index.html'));
+// });
 
 db.once('open', () => {
   app.listen(PORT, () => {
