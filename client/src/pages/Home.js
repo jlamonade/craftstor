9c05344457fd895211c7b884cbb8f6720b7dfdc9
Hekwang Lhi <hekwang.lhi@gmail.com>
2021-06-24 13:57:03 -0400

    home: no dashboard

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,18 +1,52 @@
 import React from 'react'
-import { Container } from '@material-ui/core'
 import Dashboard from './Dashboard'
 import Auth from '../utils/auth'
  
+//  added
+// import { makeStyles } from '@material-ui/core/styles';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Container from '@material-ui/core/Container';
+import MainFeaturedPost from './MainFeaturedPost';
+import Footer from './Footer';
+
+
+// const useStyles = makeStyles((theme) => ({
+//   mainGrid: {
+//     marginTop: theme.spacing(3),
+//   },
+// }));
+// >>>>>>>>>>>>>>>> added
+
+const mainFeaturedPost = {
+  title: 'CraftSor',
+  description:
+    "Find a ....... s.",
+  image: 'https://source.unsplash.com/random',
+  imgText: 'main image description',
+  linkText: '…',
+};
+
 const Home = () => {
+  // const classes = useStyles();
 
   return (
+    <React.Fragment>
+    <CssBaseline />
     <Container>
       {Auth.loggedIn() ? (
         <Dashboard />
       ) : (
-        <div>Not dashboard</div>
+          <>
+            <Container maxWidth="lg">
+              <main>
+                <MainFeaturedPost post={mainFeaturedPost} />
+              </main>
+            </Container>
+            <Footer title="Footer" description="JPWDH Group" />
+          </>
       )}
     </Container>
+    </React.Fragment>
   )
 }
 
