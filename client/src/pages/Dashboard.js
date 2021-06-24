07256b425f969321252feb73b2c2ab678df46918
Hekwang Lhi <hekwang.lhi@gmail.com>
2021-06-24 13:07:48 -0400

    dashboard

--- client/src/pages/Dashboard.js
+++ client/src/pages/Dashboard.js
@@ -18,12 +18,9 @@ import { makeStyles } from '@material-ui/core/styles';
 
 // added
 const useStyles = makeStyles((theme) => ({
-  icon: {
-    marginRight: theme.spacing(2),
-  },
   heroContent: {
     backgroundColor: theme.palette.background.paper,
-    padding: theme.spacing(8, 0, 6),
+    padding: theme.spacing(1, 0, 1),
   },
   heroButtons: {
     marginTop: theme.spacing(4),
